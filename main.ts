3b875170452ad9e096ef9585544408724a060f0f
ksbn82 <35519990+ksbn82@users.noreply.github.com>
2021-04-08 11:20:51 +0900

    판정작성중

--- main.ts
+++ main.ts
@@ -1,16 +1,29 @@
+let 판정 = 0
 function downdown (Leftright: boolean) {
     if (Leftright == false) {
         led.toggle(0, 0)
         led.toggle(1, 0)
         basic.pause(100)
-        for (let index = 0; index <= 3; index++) {
+        for (let index = 0; index <= 2; index++) {
             NextLed(index + 1)
             basic.pause(100)
         }
+        NextLed(4)
+        판정 = 1
+        basic.pause(100)
+        판정 = 0
     }
 }
 input.onButtonPressed(Button.A, function () {
-	
+    if (판정 == 1) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    }
 })
 function NextLed2 (num: number) {
     led.toggle(4, num - 1)
