abd8117967652a94d81f6f558208cdcda9c0f359
ksbn82 <35519990+ksbn82@users.noreply.github.com>
2021-04-15 10:15:57 +0900

    판정개선

--- main.ts
+++ main.ts
@@ -1,28 +1,30 @@
+let 속도 = 0
 let 판정 = 0
 function downdown (Leftright: boolean) {
+    속도 = 100
     if (Leftright == false) {
         led.toggle(0, 0)
         led.toggle(1, 0)
-        basic.pause(100)
-        for (let index = 0; index <= 2; index++) {
-            NextLed(index + 1)
-            basic.pause(100)
-        }
-        NextLed(4)
+        basic.pause(속도)
+        NextLed(1)
+        basic.pause(속도)
+        NextLed(2)
+        basic.pause(속도)
         판정 = 1
-        basic.pause(100)
+        NextLed(3)
+        basic.pause(속도)
+        NextLed(4)
+        basic.pause(속도)
         판정 = 0
     }
 }
 input.onButtonPressed(Button.A, function () {
     if (판정 == 1) {
-        basic.showLeds(`
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            # # # # #
-            `)
+        for (let index = 0; index <= 4; index++) {
+            for (let inbdex2 = 0; inbdex2 <= 4; inbdex2++) {
+                led.plot(inbdex2, index)
+            }
+        }
     }
 })
 function NextLed2 (num: number) {
